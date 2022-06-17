c25d921f7811186dda3dfacb05b4ea14381bcea1
asyms <asymons@micaszb03.esat.kuleuven.be>
2022-06-17 17:24:39 +0200

    change savestage filename

--- classes/stages/SaveStage.py
+++ classes/stages/SaveStage.py
@@ -81,7 +81,7 @@ class SimpleSaveStage(Stage):
         
         for id, (cme, extra_info) in enumerate(substage.run()):
             cme: CostModelEvaluation
-            filename = self.dump_filename_pattern.format(datetime=datetime.now().isoformat())
+            filename = self.dump_filename_pattern.format(datetime=datetime.now().isoformat().replace(":", "-"))
             self.save_to_json(cme, filename=filename)
             logger.info(f"Saved CostModelEvaluation with energy {cme.energy_total:.3e} and latency {cme.latency_total2:.3e} to {filename}")
             yield cme, extra_info
--- inputs/workshop/workload.py
+++ inputs/workshop/workload.py
@@ -1,10 +1,10 @@
 workload = {
-    0: {'equation': 'O[k][b]+=A[k][c]*B[c][b]',
+    0: {'equation': 'O[k][b]+=W[k][c]*I[c][b]',  # O should be the name of the output operand
         'loop_dim_size': {'K': 4, 'B': 4, 'C': 4},
-        'operand_precision': {'O': 16, 'O_final': 8, 'A': 8, 'B': 8},
+        'operand_precision': {'O': 16, 'O_final': 8, 'W': 8, 'I': 8},  # O = partial sum precision
         'core_allocation': 1,
         'spatial_mapping': {'D1': ('K', 4)},  # Must match with the dimensions of core 1
         'temporal_ordering': [('B', 4), ('C', 4)],
-        'memory_operand_links': {'O': 'O', 'A': 'I1', 'B': 'I2'}
+        'memory_operand_links': {'O': 'O', 'W': 'I1', 'I': 'I2'}
         }
 }
